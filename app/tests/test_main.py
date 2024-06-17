4298f0b187ab5740e332ccdf34ffdf44850406b3
Amir Hossein Moayedi <amir.h.moayedi@gmail.com>
2024-06-17 17:36:30 +0330

    test: fix the test duo to  changes in fastapi application

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
-from app.main import APP
+from app.presentation.application import get_app
 
-client = TestClient(APP)
+client = TestClient(get_app())
 
 
 class TestApp:
@@ -9,4 +9,4 @@ class TestApp:
     def test_root(self):
         response = client.get("/")
         assert response.status_code == 200
-        assert response.json() == {"message": "Hello World"}
+        assert response.json() == {"message": "hello world"}
