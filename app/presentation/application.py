55e7a8b95d39e61f71bd7140c7317107e1b5c920
Amir Hossein Moayedi <amir.h.moayedi@gmail.com>
2024-06-18 18:55:36 +0330

    feat: add event shutdown/startup events and main rout to main app

--- app/presentation/application.py
+++ app/presentation/application.py
@@ -1,15 +1,33 @@
-from typing import Dict
-
 from fastapi import FastAPI
+from fastapi.responses import UJSONResponse
+
+from app.settings import settings
+
+from app.presentation.apis.router import api_router
+from app.presentation.lifetime import register_startup_event, register_shutdown_event
 
 
-def get_app() -> FastAPI:  # todo: complete this
+def get_app() -> FastAPI:
+    """
+    Get FastAPI application.
+
+    This is the main constructor of an application.
+
+    :return: application.
+    """
+    #
     app = FastAPI(
         title="fastapi template",
+        version=settings.version,
+        docs_url=None,
+        redoc_url=None,
+        openapi_url="/api/openapi.json",
+        default_response_class=UJSONResponse,
     )
 
-    @app.get("/")
-    def root() -> Dict[str, str]:
-        return {"message": "hello world"}
+    register_startup_event(app)
+    register_shutdown_event(app)
+
+    app.include_router(router=api_router, prefix="/api")
 
     return app
