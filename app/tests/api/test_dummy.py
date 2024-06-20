bbe35396f0efe13e8bed008490683bc39ea3c65d
Amir Hossein Moayedi <amir.h.moayedi@gmail.com>
2024-06-20 16:16:49 +0330

    feat: complete the test for dummy apis

--- app/tests/api/test_dummy.py
+++ app/tests/api/test_dummy.py
@@ -1,45 +1,40 @@
-from unittest.mock import MagicMock
+from datetime import datetime
 
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from app.presentation.application import get_app
-from app.service import DummyService
-
-
-@pytest.fixture
-def client():
-    return TestClient(get_app())
-
-
-@pytest.fixture
-def mock_dummy_service():
-    return MagicMock(spec=DummyService)
+from app.tests.factories.dummy import DummyModelFactory
 
 
 class TestDummyView:
-    def test_get_dummies(self, client: TestClient):
-        response = client.get("/api/v1/dummy/")
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == [
-            {"id": 1, "name": "Dummy 1"},
-            {"id": 2, "name": "Dummy 2"},
-        ]
+    @pytest.fixture(autouse=True)
+    async def setup(self, client: TestClient, dbsession: AsyncSession):
+        # setup for this fixture
+        DummyModelFactory.create_batch(10)
+        yield
+        # teardown for this fixture
+        DummyModelFactory.clean_up()
 
-    def test_create_dummy_model(self, client: TestClient):
+    @pytest.mark.anyio
+    async def test_create_dummy_model(self, client: TestClient, dbsession: AsyncSession):
         dummy_data = {"name": "New Dummy"}
         response = client.post("/api/v1/dummy/", json=dummy_data)
         assert response.status_code == status.HTTP_201_CREATED
+        assert response.json().get("id") == 11
+        assert response.json().get("name") == "New Dummy"
+        assert (
+            datetime.strptime(response.json().get("created_at"), "%Y-%m-%dT%H:%M:%S.%f").date()
+            == datetime.today().date()
+        )
 
-    def test_get_dummy_model(self, client: TestClient):
-        dummy_id = 1
-        response = client.get(f"/api/v1/dummy/{dummy_id}")
+    @pytest.mark.anyio
+    async def test_get_dummies(self, client: TestClient, dbsession: AsyncSession):
+        response = client.get("/api/v1/dummy")
         assert response.status_code == status.HTTP_200_OK
-        assert response.json() == {"id": dummy_id, "name": "Dummy 1"}
-
-    def test_update_dummy_model(self, client: TestClient):
-        dummy_id = 1
-        dummy_data = {"name": "Updated Dummy"}
-        response = client.patch(f"/api/v1/dummy/{dummy_id}", json=dummy_data)
-        assert response.status_code == status.HTTP_200_OK
+        assert len(response.json()) == 10
+        assert response.json()[0].get("id") is not None
+        assert response.json()[0].get("name") is not None
+        assert response.json()[0].get("created_at") is not None
+        assert response.json()[0].get("updated_at") is not None
