9e54684ef42d728b95ef9c62db2b2866008c695b
Amir Hossein Moayedi <amir.h.moayedi@gmail.com>
2024-06-24 01:03:14 +0330

    fix: prefix for the main router is now set to empty string

--- app/presentation/apis/v1/router.py
+++ app/presentation/apis/v1/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.presentation.apis.v1 import dummy, docs
 
-router = APIRouter(prefix="/v1")
+router = APIRouter()
 
 router.include_router(docs.router)
 router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
